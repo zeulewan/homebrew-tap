2c5fe21439aaceee8bf3fcad7c460661a59c7ca7
Zeulewan Mordasiewicz <zeul@mordasiewicz.com>
2026-05-16 22:29:53 -1000

    Add WebFinder cask

---
+++ Casks/webfinder.rb
@@ -0,0 +1,24 @@
+cask "webfinder" do
+  version "1.3.2"
+  sha256 "e97c73d2252f048cf0373fcade4b9636c35a1a7095543612e53fdd81dd5569a0"
+
+  url "https://github.com/zeulewan/web-finder/releases/download/v#{version}/WebFinder.app.zip",
+      verified: "github.com/zeulewan/web-finder/"
+  name "WebFinder for Tailscale"
+  desc "Discover WebFinder-published web services on Tailscale"
+  homepage "https://github.com/zeulewan/web-finder"
+
+  depends_on macos: ">= :ventura"
+
+  app "WebFinder.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.zeul.web-finder",
+    "~/Library/Application Support/WebFinder",
+    "~/Library/Caches/com.zeul.web-finder",
+    "~/Library/Caches/WebFinder",
+    "~/Library/HTTPStorages/com.zeul.web-finder",
+    "~/Library/Preferences/com.zeul.web-finder.plist",
+    "~/Library/Saved Application State/com.zeul.web-finder.savedState",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,31 @@
+# Zeul Homebrew Tap
+
+Homebrew tap for WebFinder and related tools.
+
+## Install
+
+```bash
+brew tap zeulewan/tap
+brew install --cask webfinder
+```
+
+Or without tapping first:
+
+```bash
+brew install --cask zeulewan/tap/webfinder
+```
+
+## Packages
+
+### Casks
+
+- `webfinder` - macOS menu bar app for discovering WebFinder-published services on Tailscale.
+
+## Update / Uninstall
+
+```bash
+brew update
+brew upgrade --cask webfinder
+brew uninstall --cask webfinder
+```
+
