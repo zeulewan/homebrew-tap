e3cc7f53b0f27744ca79fc163d3bf9a5a324622d
zeulewan <zeul@mordasiewicz.com>
2026-05-22 07:27:01 -0400

    Update web-finder formula to 1.3.7

--- Formula/web-finder.rb
+++ Formula/web-finder.rb
@@ -1,8 +1,8 @@
 class WebFinder < Formula
   desc "Discover Tailscale-served web interfaces with WebFinder"
   homepage "https://github.com/zeulewan/web-finder"
-  url "https://github.com/zeulewan/web-finder/archive/refs/tags/v1.3.6.tar.gz"
-  sha256 "4648bd2f8d7c59dc190ca50295f344e39a94803a64c84cc659fbe1c379048239"
+  url "https://github.com/zeulewan/web-finder/archive/refs/tags/v1.3.7.tar.gz"
+  sha256 "c5febc5b66fadbcc2a45bf39558bb79f6df1d71cd13bb09decde6b94a7b4125f"
   license "MIT"
 
   depends_on "node"
